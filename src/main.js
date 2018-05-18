adb4d33f9bfeea7c247b376e73d59234d9389c37
RidhoBeteer <ridho.beteer@gmail.com>
2018-05-19 02:59:37 +0700

    add decrypt"

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-var key, plain, i;
+var key, plain = "", i;
 var j = 0;
 var cipher = "";
 var unicodeCipher;
 
 // String.fromCharCode(n) & var.charCodeAt(n);
-
+// Encrypt
 function capture() {
 
   key = document.getElementById('key').value.toUpperCase();
@@ -36,4 +36,40 @@ function encrypt(plain, key) {
     j++;
   }
   document.getElementById('cipherInput').value = cipher.toLowerCase();
+}
+
+//Decrypt
+
+function getCipher() {
+  key = document.getElementById('key').value.toUpperCase();
+  cipher = document.getElementById('cipher').value.toUpperCase();
+
+  decrypt(cipher, key);
+}
+
+function decrypt(cipher, key) {
+  var max = key.length;
+
+  for(i = 0; i < cipher.length; i++) {
+    var c = cipher.charAt(i);
+    var unicodeCipher = c.charCodeAt(0);
+
+    if(j >= max) {
+      j = 0;
+    }
+
+    if(unicodeCipher == 32) {
+      plain += "";
+    } else if(unicodeCipher > 65 || unicodeCipher < 90) {
+      unicodeChar = (unicodeCipher - key.charCodeAt(j))%26 + 65;
+      if(unicodeChar < 65) {
+        unicodeChar += 26;
+      }
+      plain += String.fromCharCode(unicodeChar);
+    }
+
+    j++;
+  }
+
+  document.getElementById('plain').value = plain.toLowerCase();
 }
